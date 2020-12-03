54f63891b7245bb30ebd4da7a20322f210ad4542
Rasmus Göransson <rasmus@volumental.com>
2020-12-03 14:31:22 +0100

    Add solutions to day 3 part 2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,32 +1,40 @@
 use std::{io, io::prelude::*};
 
 fn main() {
-    let mut x = 0;
-    let mut y = 0;
-    let mut num_trees = 0;
-
     let mut forest: Vec<_> = io::stdin()
         .lock()
         .lines()
         .map(|line| line.unwrap())
         .collect();
 
-    loop {
-        x += 3;
-        y += 1;
-        if y >= forest.len() {
-            break;
+    let mut product: i64 = 1;
+    let slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]];
+    for [dx, dy] in &slopes {
+        let mut x = 0;
+        let mut y = 0;
+        let mut num_trees = 0;
+        loop {
+            x += dx;
+            y += dy;
+            if y >= forest.len() {
+                break;
+            }
+            x = x % forest[y].len();
+            if &forest[y][x..x + 1] == "#" || &forest[y][x..x + 1] == "X" {
+                num_trees += 1;
+                forest[y] = format!("{}X{}", &forest[y][..x], &forest[y][x + 1..]);
+            } else {
+                forest[y] = format!("{}O{}", &forest[y][..x], &forest[y][x + 1..]);
+            }
         }
-        x = x % forest[y].len();
-        if &forest[y][x..x + 1] == "#" {
-            num_trees += 1;
-            forest[y] = format!("{}X{}", &forest[y][..x], &forest[y][x + 1..]);
-        } else {
-            forest[y] = format!("{}O{}", &forest[y][..x], &forest[y][x + 1..]);
-        }
+        product *= num_trees;
+        println!(
+            "Number of trees using slope right {}, down {}: {}",
+            dx, dy, num_trees
+        );
     }
+    println!("Product of number of trees: {}", product);
     for line in forest {
         println!("{}", line);
     }
-    println!("Number of trees: {}", num_trees);
 }
