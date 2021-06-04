9b4441fb3d152dbb8c6dbbf169a80f0009caa428
Rasmus Göransson <rasmus@volumental.com>
2021-06-04 22:25:47 +0200

    day 4: Break out validate_passport function

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -1,6 +1,33 @@
 use std::collections::BTreeSet;
 use std::{io, io::prelude::*};
 
+static REQUIRED_FIELDS: [&str; 7] = [
+    "byr", // (Birth Year)
+    "iyr", // (Issue Year)
+    "eyr", // (Expiration Year)
+    "hgt", // (Height)
+    "hcl", // (Hair Color)
+    "ecl", // (Eye Color)
+    "pid", // (Passport ID)
+           // "cid", // (Country ID) - optional!
+];
+
+fn validate_passport(passport: &str) -> Result<(), ()> {
+    let mut field_names = BTreeSet::new();
+    for field in passport.split_ascii_whitespace() {
+        let (field_name, _) = field
+            .split_once(":")
+            .expect("Failed to split field into name and data");
+        field_names.insert(field_name);
+    }
+    for required_field in REQUIRED_FIELDS.iter() {
+        if !field_names.contains(required_field) {
+            return Err(());
+        }
+    }
+    Ok(())
+}
+
 fn main() {
     let input = {
         let mut buffer = String::new();
@@ -11,38 +38,18 @@ fn main() {
         buffer
     };
 
-    let required_fields = {
-        let mut a = BTreeSet::new();
-        a.insert("byr"); // (Birth Year)
-        a.insert("iyr"); // (Issue Year)
-        a.insert("eyr"); // (Expiration Year)
-        a.insert("hgt"); // (Height)
-        a.insert("hcl"); // (Hair Color)
-        a.insert("ecl"); // (Eye Color)
-        a.insert("pid"); // (Passport ID)
-
-        // a.insert("cid"), // (Country ID) - optional!
-        a
-    };
-
     let mut num_valid_passports = 0;
     let mut num_invalid_passports = 0;
-    let mut i = 0;
     for passport in input.split("\n\n") {
-        let mut field_names = BTreeSet::new();
-        for field in passport.split_ascii_whitespace() {
-            let (field_name, _) = field
-                .split_once(":")
-                .expect("Failed to split field into name and data");
-            field_names.insert(field_name);
+        match validate_passport(passport) {
+            Ok(_) => {
+                num_valid_passports += 1;
+            }
+            Err(_) => {
+                num_invalid_passports += 1;
+            }
         }
-        if required_fields.is_subset(&field_names) {
-            num_valid_passports += 1;
-        } else {
-            num_invalid_passports += 1;
-        }
-        println!("Passport {}: {}", i, passport);
-        i += 1;
+        // println!("Passport {}: {}", i, passport);
     }
 
     println!("Number of valid passports: {}", num_valid_passports);
